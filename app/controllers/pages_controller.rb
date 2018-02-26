83e829ceb6ace93df7a6ff1dfbcbdbe2b1cce320
Ian Harwell <sirharwell@gmail.com>
2018-02-26 12:10:00 -0700

    create pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,4 +13,20 @@ class PagesController < ApplicationController
   def new
     @page = Page.new
   end
+
+  #POST /pages
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to page_path(@page)
+    else
+      render :new
+    end
+  end
+
+  private
+    def page_params
+      params.require(:page).permit(:body, :author, :title)
+    end
 end
